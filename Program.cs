1b9648be454b9fd50b5b374dc2c306a5f4a753d8
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-11-28 17:28:50 +0000

    completed up to create page model

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RockPaperScissorsWebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RockPaperScissorsWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RockPaperScissorsWebAppContext") ?? throw new InvalidOperationException("Connection string 'RockPaperScissorsWebAppContext' not found.")));
 
 var app = builder.Build();
 
